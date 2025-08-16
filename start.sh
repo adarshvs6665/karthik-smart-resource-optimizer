9448bcb2ef61310037a4b6013f82baf10f1d50f9
Adarsh V S <adarshvs6665@gmail.com>
2025-08-16 22:58:17 +0530

    scripts

--- start.sh
+++ start.sh
@@ -1,13 +1,12 @@
 #!/bin/bash
-# run_apps.sh
 # Script to start application
 
 mkdir -p logs
 
-# Run Flask app
-nohup python3 app.py > logs/flask.log 2>&1 &
+# Run flask app
+nohup python3 predictor.py > logs/flask.log 2>&1 &
 
-# Run Streamlit app
+# Run streamlit app
 nohup streamlit run streamlit_app.py --server.port 8501 > logs/streamlit.log 2>&1 &
 
 echo "Flask and Streamlit apps started in background."
